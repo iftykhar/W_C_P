bbbea920f84c44c3d9ad445df8e4c0c0875d6e45
iftykhar alam <s.m.ifty49@gmail.com>
2024-09-13 22:37:53 +0600

    Problem 2

--- JS/problems/main.js
+++ JS/problems/main.js
@@ -6,3 +6,12 @@ let SarahBought = 15;
 let ToatlApple = SarahHas + SarahBought;
 let AppleResult = "Sarah Has"+" "+ToatlApple+" "+"Apples"+".";
 console.log(AppleResult);
+
+// Problem 02
+//  Jhon has 500 tk. He spend 200 tk. How much does Jhon has?
+
+let JhonHas = 500;
+let JhonSpent = 200;
+let JhonTotal = JhonHas - JhonSpent;
+let JhonMoney = "Jhon Has"+" "+JhonTotal+" "+"Tk.";
+console.log(JhonMoney);
