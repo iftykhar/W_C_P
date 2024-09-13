594b1215261706f22d265f470a378ee62e8b444b
iftykhar alam <s.m.ifty49@gmail.com>
2024-09-13 22:02:31 +0600

    practicing JS

---
+++ JS/problems/main.js
@@ -0,0 +1,8 @@
+//problem 01 
+// Sarah has 25 Apples and She bought 15 apples from shop. How many apples does sarah have?
+
+let SarahHas = 25;
+let SarahBought = 15;
+let ToatlApple = SarahHas + SarahBought;
+let AppleResult = "Sarah Has"+" "+ToatlApple+" "+"Apples"+".";
+console.log(AppleResult);
