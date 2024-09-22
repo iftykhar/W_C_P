6431446ce4243121baa9bd78c7602eb5e06585a7
iftykhar alam <s.m.ifty49@gmail.com>
2024-09-22 23:27:16 +0600

    problem solving

--- JS/problems/class-hw/main.js
+++ JS/problems/class-hw/main.js
@@ -35,3 +35,41 @@ console.log(pr5);
 let pr6 = ['car','bike','bus'];
 pr6.shift();
 console.log(pr6);
+
+
+// problem 07 array ['boat','ship']
+// add 'train' and 'plane' in the begining of the array
+
+let pr7 = ['boat','ship'];
+pr7.unshift('train','plane');
+console.log(pr7);
+
+// problem 08 array  ['dog','cat','bird']
+// use 'delete' to remove the 2nd element from the array
+
+let pr8 = ['dog','cat','bird'];
+delete pr8[1];
+console.log(pr8);
+
+// problem 09 array [1,2,3] and [4,5,6]
+// merge these array
+
+const pr9A1 = [1,2,3];
+const pr9A2 = [4,5,6];
+
+let pr9 = pr9A1.concat(pr9A2);
+console.log(pr9);
+
+// problem 10 array ['a','b','c','d','e','f']
+// delete 'c' and 'd' using splice()
+
+let pr10 = ['a','b','c','d','e','f'];
+pr10.splice(2,2);
+console.log(pr10);
+
+// problem 11 array ['first','second',''third,'fourth','fifth','sixth','seventh','eighth','nineth']
+// print the element at index 4
+
+let pr11 = ['first','second','third','fourth','fifth','sixth','seventh','eighth','nineth'];
+const pr11result = pr11.at(4);
+console.log(pr11result);
