aae70958e4703b1eabe0b413764ee6e01787a246
iftykhar alam <s.m.ifty49@gmail.com>
2024-10-24 20:12:01 +0600

    JS Methods and functions

---
+++ JS/assingment/JS-methods/main.js
@@ -0,0 +1,15 @@
+//  1. Define a function with two parameters, assigning a default value to the second one. Call it with both one and two arguments, and show the outputs.
+
+function
+
+//  2. Create a function that takes a name and a city as input and returns a multi-line message using template literals, incorporating both variables.
+//  3. Refactor a given function that multiplies two numbers into an arrow function, reducing its body to the minimum.
+//  4. Combine two arrays using the spread operator and find the maximum value in the merged array.
+//  5.  Create an object with properties name, age, and country, and use destructuring to extract them into variables. Also, destructure an array of five numbers to get the first two elements.
+//  6.  Access the city property of a nested object using both dot notation and optional chaining.
+//  7.  Given an array of student objects with properties name and marks, use:map() to extract the names.
+//  8.  filter() to return students with marks above 80.
+//  9.  find() to get the first student with marks over 90.
+//  10.  Use forEach() on an array of objects (e.g., products with name and price) to log a message with each product's details.
+//  11.  Use map() on a string array to create a new array where each string is prefixed with "Hello, ".
+//  12. Given an array of objects, use filter() to return all objects where a specific condition is met (e.g., products priced above a certain amount).
